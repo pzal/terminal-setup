f44f00ecc5c0cc507decf20f13287aa90f3104ef
Piotr Zalewski <mail@pzalewski.com>
2025-06-04 19:32:09 +0200

    nvim config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  -- add catppuccin
+  { 
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin"
+  },
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-latte",
+    },
+  }
+}
+
+
