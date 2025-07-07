cecbb5f5f22c597003b52e0c8dcfa148a89787b3
Piotr Zalewski <mail@pzalewski.com>
2025-07-07 16:56:08 +0200

    fixed git hunks

--- nvim/init.lua
+++ nvim/init.lua
@@ -229,13 +229,13 @@ require('lazy').setup({
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
+      -- signs = {
+      --   add = { text = '+' },
+      --   change = { text = '~' },
+      --   delete = { text = '_' },
+      --   topdelete = { text = '‾' },
+      --   changedelete = { text = '~' },
+      -- },
     },
     on_attach = function(bufnr)
       local gitsigns = require 'gitsigns'
@@ -253,7 +253,7 @@ require('lazy').setup({
         else
           gitsigns.nav_hunk 'next'
         end
-      end)
+      end, { desc = 'Next git hunk' })
 
       map('n', '[c', function()
         if vim.wo.diff then
@@ -261,46 +261,46 @@ require('lazy').setup({
         else
           gitsigns.nav_hunk 'prev'
         end
-      end)
+      end, { desc = 'Previous git hunk' })
 
       -- Actions
-      map('n', '<leader>hs', gitsigns.stage_hunk)
-      map('n', '<leader>hr', gitsigns.reset_hunk)
+      map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Stage hunk' })
+      map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Reset hunk' })
 
       map('v', '<leader>hs', function()
         gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-      end)
+      end, { desc = 'Stage hunk' })
 
       map('v', '<leader>hr', function()
         gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-      end)
+      end, { desc = 'Reset hunk' })
 
-      map('n', '<leader>hS', gitsigns.stage_buffer)
-      map('n', '<leader>hR', gitsigns.reset_buffer)
-      map('n', '<leader>hp', gitsigns.preview_hunk)
-      map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+      map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Stage buffer' })
+      map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Reset buffer' })
+      map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Preview hunk' })
+      map('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'Preview hunk inline' })
 
       map('n', '<leader>hb', function()
         gitsigns.blame_line { full = true }
-      end)
+      end, { desc = 'Blame line' })
 
-      map('n', '<leader>hd', gitsigns.diffthis)
+      map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Diff this' })
 
       map('n', '<leader>hD', function()
         gitsigns.diffthis '~'
-      end)
+      end, { desc = 'Diff this (cached)' })
 
       map('n', '<leader>hQ', function()
         gitsigns.setqflist 'all'
-      end)
-      map('n', '<leader>hq', gitsigns.setqflist)
+      end, { desc = 'Send all hunks to quickfix' })
+      map('n', '<leader>hq', gitsigns.setqflist, { desc = 'Send hunks to quickfix' })
 
       -- Toggles
-      map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-      map('n', '<leader>tw', gitsigns.toggle_word_diff)
+      map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Toggle git blame' })
+      map('n', '<leader>tw', gitsigns.toggle_word_diff, { desc = 'Toggle word diff' })
 
       -- Text object
-      map({ 'o', 'x' }, 'ih', gitsigns.select_hunk)
+      map({ 'o', 'x' }, 'ih', gitsigns.select_hunk, { desc = 'Select hunk' })
     end,
   },
 
@@ -1266,7 +1266,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-  require 'dap'
+  require 'dap',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
