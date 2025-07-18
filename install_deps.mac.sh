06b9e9ed56399d60524e56a2ba35c0aa79f8a64b
Piotr Zalewski <33444111+pzal@users.noreply.github.com>
2025-07-18 11:55:40 +0200

    Update install_deps.mac.sh

--- install_deps.mac.sh
+++ install_deps.mac.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 xcode-select --install
 brew install \
-  jesseduffield/lazygit/lazygit curl fzf ripgrep fd coreutils neovim tmux pipx nnn unzip
+  jesseduffield/lazygit/lazygit curl ripgrep fd coreutils neovim tmux pipx nnn unzip
 pipx ensurepath
 sudo pipx ensurepath --global
